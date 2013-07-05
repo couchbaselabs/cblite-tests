a653df49527d971a4956df8d2bd759ecc7b585c2
Tommie McAfee <tommie@couchbase.com>
2013-07-05 07:57:35 -0700

    interact with launchers via restful http frontend

--- config/local.js
+++ config/local.js
@@ -1,8 +1,13 @@
 var path = require("path");
 
-module.exports = {
-  LiteServPath : "/Users/jchris/Library/Developer/Xcode/DerivedData/CouchbaseLite-bhhkqfrlnbnqogachbjesrrwggdn/Build/Products/Debug/LiteServ",
-  SyncGatewayPath : "/Users/jchris/code/cb/mobile/sync_gateway/bin/sync_gateway"
+var config = module.exports = {
+  LiteServPath : "/Users/tmcafee/Library/Developer/Xcode/DerivedData/CouchbaseLite-hkgaefkqnugcoubpcomqikwhdtwh/Build/Products/Debug/LiteServ",
+  SyncGatewayPath : "/Users/tmcafee/sandbox/sync_gateway/bin/sync_gateway",
+  Backends : ["walrus:"],
+  ListenerIP : "10.32.38.94",
+  ListenerPort : 8180,
+  LiteServBasePort : 59850,
+  SyncGatewayBasePort : 9888
 }
 
 module.exports.SyncGatewayAdminParty = __dirname+"/admin_party.json"
