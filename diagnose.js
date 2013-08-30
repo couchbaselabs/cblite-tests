b162a1d527525148b82114e8acd4259829f4e1c9
Chris Anderson <jchris@couchbase.com>
2013-08-30 13:24:17 -0700

    update diagnose script

--- diagnose.js
+++ diagnose.js
@@ -1,7 +1,10 @@
 var coax = require("coax")
 
+// var dbURL = "http://sync.couchbasecloud.com:4984/guestok67/"
 
-var db = coax("http://sync.couchbasecloud.com:4984/guestok92/")
+var dbURL = "http://localhost:4984/db/"
+
+var db = coax(dbURL)
 
 
 db("_changes", function (err, data) {
@@ -15,7 +18,28 @@ db("_changes", function (err, data) {
         missing.push(d.id)
       }
     })
+
+    var changeIds = {}, dupIds = [];
+    var changeSeqs = {}, dupSeqs = [];
+
+    data.results.forEach(function(r){
+      if (changeIds[r.id]) {
+        dupIds.push(r.id)
+      }
+      changeIds[r.id] = true
+
+      if (changeSeqs[r.seq]) {
+        dupSeqs.push(r.seq)
+      }
+      changeSeqs[r.seq] = true
+    })
+
+
+
     console.log("missing "+missing.length+", ids:", missing.join(', '))
+    console.log("duplicate change ids "+dupIds.length+", ids:", dupIds.join(', '))
+    console.log("duplicate change seqs "+dupSeqs.length+", seqs:", dupSeqs.join(', '))
+
 
   })
 
