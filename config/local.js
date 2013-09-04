a40f826aa3db123f6d78566a2d621613140eb8c6
Tommie McAfee <tommie@couchbase.com>
2013-09-04 13:50:58 -0700

    moving environment config logic to eof

--- config/local.js
+++ config/local.js
@@ -1,8 +1,8 @@
 var path = require("path");
 
-var localIP = process.env.LOCAL_IP || '127.0.0.1'
-var lsProviders = ['http://'+localIP]
-var pouchProviders = []
+var localIP, lsProviders, pouchProviders = null
+
+envSetup()
 
 var config = module.exports = {
   LiteServPath      : process.env.LITESERV_PATH,
@@ -14,14 +14,8 @@ var config = module.exports = {
   TestEndpoint      : "ios"  // ios, android, pouchdb, couchdb
 }
 
-if(process.env.LITESERV_PROVIDERS)
-  lsProviders =  process.env.LITESERV_PROVIDERS.split(',')
-
-if(process.env.POUCH_PROVIDERS)
-  pouchProviders =  process.env.POUCH_PROVIDERS.split(',')
-
 module.exports.resources = {
-  LiteServProviders   : lsProviders,  // array of client providers
+  LiteServProviders   : lsProviders,  // array of other Listeners...ie [http://10.10.1.1:8189, http://...]
   PouchDBProviders    : pouchProviders,
   SyncGatewayProvider : "http://127.0.0.1:8189",
   Provision           : false
@@ -32,3 +26,24 @@ module.exports.resources = {
  * the default admin_party.json will use "walrus" on bucket "db"
  */
 module.exports.SyncGatewayAdminParty = __dirname+"/admin_party.json"
+
+
+/*
+ ****** ENV CONFIG *****
+ */
+function envSetup(){
+
+  localIP = process.env.LOCAL_IP || '127.0.0.1'
+  lsProviders = ['http://'+localIP]
+  pouchProviders = []
+
+  // ie. export LITESERV_PROVIDERS="http://127.0.0.1:8189,http://127.0.0.2:8189,http://127.0.0.3:8189"
+  if(process.env.LITESERV_PROVIDERS != undefined)
+    lsProviders =  process.env.LITESERV_PROVIDERS.split(',')
+      .filter(function(serv){ if (serv !='' ) return true })
+
+  if(process.env.POUCH_PROVIDERS != undefined)
+    pouchProviders =  process.env.POUCH_PROVIDERS.split(',')
+      .filter(function(serv){ if (serv !='' ) return true })
+
+}
