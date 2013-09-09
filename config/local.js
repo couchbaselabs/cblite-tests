8f90347dda02a8098c4d4aa28d182bb6b6625f02
Tommie McAfee <tommie@couchbase.com>
2013-09-08 19:42:47 -0700

    move perf logic from config file
    moving information about providers to perf.js
    to clean up local.js used for functional testing

--- config/local.js
+++ config/local.js
@@ -1,24 +1,13 @@
 var path = require("path");
 
-var localIP, lsProviders, pouchProviders = null
-
-envSetup()
-
 var config = module.exports = {
   LiteServPath      : process.env.LITESERV_PATH,
   SyncGatewayPath   : process.env.SYNCGATE_PATH,
-  LocalListenerIP   : localIP,
+  LocalListenerIP   : "127.0.0.1",
   LocalListenerPort : 8189,
   DbUrl             : "walrus:",
   DbBucket          : "db",
-  TestEndpoint      : "ios"  // ios, android, pouchdb, couchdb
-}
-
-module.exports.resources = {
-  LiteServProviders   : lsProviders,  // array of other Listeners...ie [http://10.10.1.1:8189, http://...]
-  PouchDBProviders    : pouchProviders,
-  SyncGatewayProvider : "http://127.0.0.1:8189",
-  Provision           : false
+  provides          : "ios",  // ios, android, pouchdb, couchdb
 }
 
 /*
@@ -27,24 +16,3 @@ module.exports.resources = {
  */
 module.exports.SyncGatewayAdminParty = __dirname+"/admin_party.json"
 module.exports.SyncGatewaySyncFunctionTest = __dirname+"/sync_function_test.json"
-
-
-/*
- ****** ENV CONFIG *****
- */
-function envSetup(){
-
-  localIP = process.env.LOCAL_IP || '127.0.0.1'
-  lsProviders = ['http://'+localIP]
-  pouchProviders = []
-
-  // ie. export LITESERV_PROVIDERS="http://127.0.0.1:8189,http://127.0.0.2:8189,http://127.0.0.3:8189"
-  if(process.env.LITESERV_PROVIDERS != undefined)
-    lsProviders =  process.env.LITESERV_PROVIDERS.split(',')
-      .filter(function(serv){ if (serv !='' ) return true })
-
-  if(process.env.POUCH_PROVIDERS != undefined)
-    pouchProviders =  process.env.POUCH_PROVIDERS.split(',')
-      .filter(function(serv){ if (serv !='' ) return true })
-
-}
--- config/perf.js
+++ config/perf.js
@@ -1,13 +1,18 @@
 var perf = module.exports = {
-  numClients: 10,
-  gateway : null,
-  clientWriteDelay : 50,
-  channelsPerClient : 5,
-  channelsPerDoc : 5,
-  runSeconds : 200,
-  requestsPerSec: 5,
-  readRatio: 90,
+  testid : "perf_"+process.hrtime()[0],
+  numClients : 10,
+  channelsPerClient : 5,  //TODO
+  channelsPerDoc : 5,   //TODO
+  runtime: 40,
+  requestsPerSec: 1,
+  readRatio: 0,
   writeRatio: 10,
-  statInterval: 10,
-  perfdb  : null, // "http://console.couchbasecloud.com/perfdash"   use for cblite-dashboard integration
+  perfdb  : "http://127.0.0.1:8190/stats",
+  // perfdb for stat data. default=internal pouch. can be any couchdb|couchcloud"
+  workload : 'readwrite',
+  enablepull : true,
+  gateway : null, // default is to start local gateway
+  gatewaydb : "db",
+  providers : ["http://127.0.0.1:8189"],
+  db : 'test-perf',   // client db prefix
 }
