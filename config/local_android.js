5ae9a85d91e9cc2c1ab4d396d34bfc84ebb7a4a2
Andrei Baranouski <andrei.baranouski@gmail.com>
2013-10-16 14:24:39 +0300

    add local_android.js

---
+++ config/local_android.js
@@ -0,0 +1,20 @@
+var path = require("path");
+
+var config = module.exports = {
+  LiteServPath      : process.env.LITESERV_PATH,
+  SyncGatewayPath   : process.env.SYNCGATE_PATH,
+  LocalListenerIP   : process.env.LOCAL_IP || "127.0.0.1",
+  LocalListenerPort : 8189,
+  DbUrl             : "walrus:",
+  DbBucket          : "db",
+  provides          : "android",  // ios, android, pouchdb, couchdb
+  numDocs           : 10,
+  channelsPerDoc    : 1,
+}
+
+/*
+ * database information in this file will override the values in this config.
+ * the default admin_party.json will use "walrus" on bucket "db"
+ */
+module.exports.SyncGatewayAdminParty = __dirname+"/admin_party.json"
+module.exports.SyncGatewaySyncFunctionTest = __dirname+"/sync_function_test.json"
