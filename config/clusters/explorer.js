258ecd79fc982c6ed4bbb1eafe043191ad3541ef
pavel-paulau <pavel.paulau@gmail.com>
2013-11-12 11:00:58 -0800

    fix missing credentials in cluster spec

--- config/clusters/explorer.js
+++ config/clusters/explorer.js
@@ -3,8 +3,8 @@ module.exports = {
         "172.23.97.50"
     ],
     couchbase: [
-        "http://172.23.97.48:8091",
-        "http://172.23.97.49:8091"
+        "http://bucket-1:password@172.23.97.48:8091",
+        "http://bucket-1:password@172.23.97.49:8091"
     ],
     "ssh_username": "root",
     "ssh_password": "couchbase"
