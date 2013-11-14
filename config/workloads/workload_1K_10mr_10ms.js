8adba39590cd3710ee6c2285031e39b278200a99
pavel-paulau <pavel.paulau@gmail.com>
2013-11-13 16:16:15 -0800

    add quick 1K performance test

---
+++ config/workloads/workload_1K_10mr_10ms.js
@@ -0,0 +1,10 @@
+module.exports = {
+    "Hostname": null,
+    "Database": "db",
+    "DocSize": 1024,
+    "RampUpIntervalMs": 600000,
+    "RunTimeMs": 1200000,
+    "SleepTimeMs": 10000,
+    "NumPullers": 700,
+    "NumPushers": 300
+}
