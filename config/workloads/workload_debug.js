073b47360ff9ffc7ae949f9350c7df63f91ba300
pavel-paulau <pavel.paulau@gmail.com>
2013-11-14 16:01:25 -0800

    add small scale workload for debug

---
+++ config/workloads/workload_debug.js
@@ -0,0 +1,10 @@
+module.exports = {
+    "Hostname": null,
+    "Database": "db",
+    "DocSize": 1024,
+    "RampUpIntervalMs": 0,
+    "RunTimeMs": 1200000,
+    "SleepTimeMs": 5000,
+    "NumPullers": 4,
+    "NumPushers": 4
+}
