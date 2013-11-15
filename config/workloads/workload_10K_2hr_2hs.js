2e78c7a781d24844f085aee70ba120cffa0bf74e
pavel-paulau <pavel.paulau@gmail.com>
2013-11-14 18:19:26 -0800

    add workload with 10K users

---
+++ config/workloads/workload_10K_2hr_2hs.js
@@ -0,0 +1,10 @@
+module.exports = {
+    "Hostname": null,
+    "Database": "db",
+    "DocSize": 1024,
+    "RampUpIntervalMs": 7200000,
+    "RunTimeMs": 14400000,
+    "SleepTimeMs": 10000,
+    "NumPullers": 7000,
+    "NumPushers": 3000
+}
