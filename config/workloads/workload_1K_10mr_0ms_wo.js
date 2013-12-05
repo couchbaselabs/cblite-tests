8f3be96dcaccb0ac1b46399f6232d6a51a51154c
pavel-paulau <pavel.paulau@gmail.com>
2013-12-05 15:14:34 -0800

    add write-only workload

---
+++ config/workloads/workload_1K_10mr_0ms_wo.js
@@ -0,0 +1,10 @@
+module.exports = {
+    "Hostname": null,
+    "Database": "db",
+    "DocSize": 1024,
+    "RampUpIntervalMs": 600000,
+    "RunTimeMs": 660000,
+    "SleepTimeMs": 5000,
+    "NumPullers": 0,
+    "NumPushers": 1000
+}
