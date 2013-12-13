0e38c7310b05b6059910a7326c872bf2ee120cda
pavel-paulau <pavel.paulau@gmail.com>
2013-12-13 12:15:34 -0800

    add 10h ramp up test

---
+++ config/workloads/workload_2K_30mr_10hs.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "Hostname": null,
+    "Database": "db",
+    "DocSize": 1024,
+    "RampUpIntervalMs": 1200000,
+    "RunTimeMs": 36000000,
+    "SleepTimeMs": 10000,
+    "NumPullers": 1400,
+    "NumPushers": 600,
+    "SerieslyHostname": "cbmonitor.sc.couchbase.com",
+    "SerieslyDatabase": null
+}
