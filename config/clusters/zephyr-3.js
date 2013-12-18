8f0990b5ae4f8b55e19c68feacb0375e86baedf4
pavel-paulau <pavel.paulau@gmail.com>
2013-12-18 13:13:29 -0800

    add clusters with 3 and 4 nodes
    and additional workload for 8K users

---
+++ config/clusters/zephyr-3.js
@@ -0,0 +1,14 @@
+module.exports = {
+    cluster: "zephyr",
+    entry_point: "172.23.97.101",
+    sync_gateway: [
+        "172.23.96.66",
+        "172.23.96.67",
+        "172.23.96.68"
+    ],
+    couchbase: [
+        "http://bucket-1:password@172.23.96.64:8091"
+    ],
+    "ssh_username": "root",
+    "ssh_password": "couchbase"
+}
