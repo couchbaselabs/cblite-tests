b0f2eb2d1d40da22935a08a40344f99dbce32e5d
Andrei Baranouski <andrei.baranouski@gmail.com>
2014-07-06 12:22:54 +0300

    fix cbl-replication.js with CB cluster

--- tests/cbl-replication.js
+++ tests/cbl-replication.js
@@ -245,7 +245,7 @@ test("verify local-replicated in dbs: 0", test_conf, function(t){
 
 test("cleanup cb bucket", function(t){
     if (config.DbUrl.indexOf("http") > -1){
-    coax.post([config.DbUrl, "pools/default/buckets/", config.DbBucket, "controller/doFlush"],
+	coax.post([config.DbUrl + "/pools/default/buckets/" + config.DbBucket + "/controller/doFlush"],
 	    {"auth":{"passwordCredentials":{"username":"Administrator", "password":"password"}}}, function (err, js){
 	      t.false(err, "flush cb bucket")
 	    });
