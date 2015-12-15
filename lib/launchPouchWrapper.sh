8204d4e9953cf351d88361c714bcf9ca32ae5bc9
Traun Leyden <tleyden@couchbase.com>
2015-12-14 17:34:16 -0800

    THIS IS DEPRECATED, DO NOT USE -- SEE https://github.com/couchbaselabs/sync-gateway-tests

--- lib/launchPouchWrapper.sh
+++
@@ -1,5 +0,0 @@
-mkdir -p tmp
-rm -rf tmp/$1
-mkdir tmp/$1
-cd tmp/$1
-../../node_modules/pouchdb-server/bin/pouch $1
--- tests/channels-changes.js
+++
@@ -1,4 +0,0 @@
-// this writes documents to the database and watches the changes feed
-// it can also validate channels for sync gateway
-// it can read changes from a sync gateway and write to a couchbase lite
-// or write directly to the sync gateway
